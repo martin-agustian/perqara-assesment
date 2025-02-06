393b34de7006d025433881f047634731f5ae5c6b
martin <martinagustian@yahoo.com>
2025-02-06 20:52:48 +0700

    add navbar

---
+++ components/container.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full md:w-[720px] lg:w-[960px] xl:w-[1140px] px-[15px] py-[20px] mx-auto">
+    <slot />
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "baltic-sea": "#24282F",
+        "ebony-clay": "#292E36",
+        "mercury": "#E5E5E5"
+      }
+    },
+  },
+  plugins: [],
+}
+
