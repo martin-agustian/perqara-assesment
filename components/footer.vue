a503a5e89861b355043387dcc1c7a43c5e560339
martin <martinagustian@yahoo.com>
2025-02-06 21:26:06 +0700

    add footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    footer
+  <div class="bg-mirage text-[12px] text-olso-grey p-[30px]">
+    <Container class="flex justify-between items-center">
+      <div>
+        © 2021 MoovieTime. All rights reserved.
+      </div>
+      <img src="../public/logo-grey.svg" class="cursor-pointer w-[88px]">      
+      <div>
+        Made with Nuxt JS (ver 3)
+      </div>
+    </Container>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,11 @@ export default {
     extend: {
       colors: {
         "baltic-sea": "#24282F",
+        "davy-grey": "#505357",
         "ebony-clay": "#292E36",
-        "mercury": "#E5E5E5"
+        "olso-grey": "#929292",
+        "mercury": "#E5E5E5",
+        "mirage": "#15191F"
       }
     },
   },
